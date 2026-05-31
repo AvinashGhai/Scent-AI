6ff3b044af39af18250d7b435ad4d022d875923a
Avinash Ghai <avinashkumarghai@gmail.com>
2026-06-01 00:15:04 +0530

    updated App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 import Discover from "./pages/Discover"
 import PerfumePage from "./pages/PerfumePage"
 import ScentMixer from "./components/ai/ScentMixer"
-import Home from "./pages/Home"
+import { AIAssistantPage } from "./components/ai/AIAssistant"
+import { AIAssistantWidget } from "../src/components/ai/AIAssistant"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <AIAssistantWidget />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/discover" element={<Discover />} />
         <Route path="/perfume/:id" element={<PerfumePage />} />
         <Route path="/mix" element={<ScentMixer />} />
+        <Route path="/assistant" element={<AIAssistantPage />} />
       </Routes>
     </BrowserRouter>
   )
